5805674f3f502af2414817cde62df3c73c821b28
Diogo Almeida <diogoa@kth.se>
2017-06-16 16:21:20 +0200

    Revamped the package to integrate with the camera robot calibration package

--- src/calibration_publisher.cpp
+++ src/calibration_publisher.cpp
@@ -1,59 +1,58 @@
 #include <ros/ros.h>
-#include <sensor_msgs/JointState.h>
+#include <yaml-cpp/yaml.h>
 #include <tf/transform_broadcaster.h>
-
 #include <fstream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-using namespace std;
+struct passwd *pw = getpwuid(getuid()); // Get the user's home directory
+std::string home_dir(pw->pw_dir);
 
 int main(int argc, char** argv) {
-
-
     ros::init(argc, argv, "calibration_publisher");
     ros::NodeHandle n("~");
-
-    string calibration_file;
-    string source_frame;
-    string target_frame;
-
-    n.param<string>("calibration_file",calibration_file, "");
-    n.param<string>("source_frame",source_frame, "");
-    n.param<string>("target_frame",target_frame, "");
+    YAML::Node params_node;
+    std::string calibration_file, camera_frame;
+    std::vector<double> pose(7);
     
-    cout<<"Calibration file "<<calibration_file<<";   source frame "<<source_frame<<";  target frame "<<target_frame<<endl;
+    calibration_file = home_dir + "/.ros/camera_info" + ros::names::parentNamespace(n.getNamespace()) + "_extrinsics.yaml";
+    std::cout << "Namespace " << ros::names::parentNamespace(n.getNamespace()) << std::endl;
+    std::cout << "Calibration file "<< calibration_file << std::endl;
 
-    ifstream file_in;
-    file_in.open(calibration_file.c_str());
-
-    double tx = 0.0, ty = 0.0, tz = 0.0, qx = 0.0, qy = 0.0, qz = 0.0, qw = 1.0;
-
+    // std::ifstream file_in;
+    // file_in.open(calibration_file.c_str());
+    
+    params_node = YAML::LoadFile(calibration_file);
+    
+    if (!n.getParam("camera_frame", camera_frame))
+    {
+      ROS_ERROR("No camera frame parameter was passed to the calibration publisher");
+      return -1;
+    }
+    
+    for (auto i = 0; i < params_node["pose"].size(); i++)
+    {
+      pose[i] = params_node["pose"][i].as<double>();
+    }
+    
+    std::cout << "yaml: " << params_node << std::endl;
+    std::cout << "pose: " << params_node["pose"] << std::endl;
+    std::cout << "base_frame: " << params_node["base_frame"] << std::endl;
+    
     tf::TransformBroadcaster br;
     tf::Transform transform;
-
+    
     // initialize transform with identity
-    transform.setOrigin( tf::Vector3(tx, ty, tz) );
-    tf::Quaternion q(qx, qy, qz, qw);
+    transform.setOrigin(tf::Vector3(pose[0], pose[1], pose[2]) );
+    tf::Quaternion q(pose[3], pose[4], pose[5], pose[6]);
     transform.setRotation(q);
-
-    if (!file_in.is_open()){
-        ROS_ERROR_STREAM("Could not open calibration file  "<<calibration_file);
-            ROS_INFO_STREAM("Publishing identity transform from between "<<source_frame<<" and "<<target_frame);
-    } else {
-        ROS_INFO_STREAM("Publishing transform from "<< calibration_file<<"between "<<source_frame<<" and "<<target_frame);
-        file_in >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
-        transform.setOrigin( tf::Vector3(tx, ty, tz) );
-        tf::Quaternion q(qx, qy, qz, qw);
-        transform.setRotation(q);
-    }
-
     ros::Rate rate(50.0f);
-
+    
     while (n.ok()) {
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), source_frame, target_frame));
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), params_node["base_frame"].as<std::string>(), camera_frame));
         rate.sleep();
     }
     
-    ROS_INFO_STREAM("Stopping publishing transform between "<<source_frame<<" and "<<target_frame);
-
     return 0;
 }
